858af1eacbc1702ec802f6c6b311dc3115d40c59
swethav08711 <shwe08711@gmail.com>
2023-01-22 15:06:23 +0530

    style story section and install tailing scollbar

--- components/Story.js
+++ components/Story.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Story({story}) {
   return (
-    <div>
-        <img src={story.img} alt={story.username}/>
-        <p>{story.username}</p>
+    <div >
+        <img className='h-14 rounded-full  p-[1.5px] border-red-500 border-2 cursor-pointer hover:scale-110 transit duration-200 ease-out' src={story.img} alt={story.username}/>
+        <p className=' text-xs w-14 truncate'>{story.username}</p>
     </div>
   )
 }
